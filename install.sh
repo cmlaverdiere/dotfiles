8565f80a2586a101e68599aaf9e08e83e1183b23
Chris Laverdiere <cmlaverdiere@gmail.com>
2016-06-25 12:31:24 -0400

    Migrate to gnu stow

--- install.sh
+++ install.sh
@@ -1,28 +1,6 @@
-echo "Installing dotfiles..."
+# Use 'stow <config>' to install respective dotfiles.
+# This is just for extra steps to be run after.
 
-cp emacs ~/.emacs
-cp gitconfig ~/.gitconfig
-cp spacemacs ~/.spacemacs
-cp tmux.conf ~/.tmux.conf
-cp Xdefaults ~/.Xdefaults
-cp xinitrc ~/.xinitrc
-cp xmobarrc ~/.xmobarrc
-cp zshrc ~/.zshrc
-
-mkdir -p ~/.config/fish
-cp config.fish ~/.config/fish
-cp redshift.conf ~/.config
-
-mkdir -p ~/.vim/syntax
-mkdir -p ~/.vim/bundle
-cp vimrc ~/.vim/vimrc
-cp bebop.vim ~/.vim/syntax
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-mkdir -p ~/.xmonad
-cp xmonad.hs ~/.xmonad
-
-mkdir -p ~/.tmuxinator
-cp mux/* ~/.tmuxinator
-
-echo "Done."
+# Get vim.plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
